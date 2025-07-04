84f8360409a5efe88702e447cec3a62ded3ac9e9
Bi <110574193+1RubiUwU1@users.noreply.github.com>
2025-07-04 14:36:05 -0500

    Update lista.lua

--- lista.lua
+++ lista.lua
@@ -1,4 +1,11 @@
-return {
-	"ADM_PROTOTYPE85",
-	"tual"
-}
+local HttpService = game:GetService("HttpService")
+
+local LISTA_NEGRA = { "uni", "tual", "otro" }
+
+-- ? Convertimos la tabla a texto
+local texto = HttpService:JSONEncode(LISTA_NEGRA)
+
+-- ? Guardamos el string en la variable global
+_G.LISTANEGRA = texto
+
+print(_G.LISTANEGRA)
